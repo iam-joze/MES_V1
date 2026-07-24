b26b4bf6ebf6789612809cc9ca685289753cd98c
Spire256 <mwanjesimonpeter732@gmail.com>
2026-07-24 12:50:41 +0300

    Fixed error in erp routes

--- backend/routes/erpRoutes.js
+++ backend/routes/erpRoutes.js
@@ -7,19 +7,6 @@ const router = express.Router();
 
 router.use(authenticateToken, requireRole('ERP'));
 
-router.post('/work-orders', erpController.receiveWorkOrder);
-router.get('/jobs/:id/production-data', erpController.exportProductionData);
-
-module.exports = router;
-const express = require('express');
-
-const erpController = require('../controllers/erpController');
-const { authenticateToken, requireRole } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.use(authenticateToken, requireRole('ERP'));
-
 /**
  * @swagger
  * /erp/work-orders:
